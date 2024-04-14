0e5afeeaa10462b4fad3c0e52cf0fc43ec860574
fabricioFalcao <fabriciohfd@gmail.com>
2024-04-14 19:46:15 -0300

    Refactoring requisit 4

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -3,17 +3,16 @@ from ting_file_management.file_management import txt_importer
 
 
 def process(path_file, instance: Queue):
-    new_text = txt_importer(path_file)
+    if not (new_text := txt_importer(path_file)) or any(
+        new["nome_do_arquivo"] == path_file for new in instance._queue
+    ):
+        return None
 
     new = {
         "nome_do_arquivo": path_file,
         "qtd_linhas": len(new_text),
         "linhas_do_arquivo": new_text,
     }
-
-    for item in instance._queue:
-        if item["nome_do_arquivo"] == path_file:
-            return None
 
     instance.enqueue(new)
     print(new)
