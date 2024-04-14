0ba089da3e065db9bc5417d3764a1982e8c49fd7
fabricioFalcao <fabriciohfd@gmail.com>
2024-04-14 19:23:45 -0300

    Requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+
+
+def process(path_file, instance: Queue):
+    new_text = txt_importer(path_file)
+
+    new = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(new_text),
+        "linhas_do_arquivo": new_text,
+    }
+
+    for item in instance._queue:
+        if item["nome_do_arquivo"] == path_file:
+            return None
+
+    instance.enqueue(new)
+    print(new)
 
 
 def remove(instance):
